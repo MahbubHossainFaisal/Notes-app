fa9468f32d8c5a97065eebd68bafbbf3b71ae418
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-11-22 22:09:55 +0600

    Global object

---
+++ global.js
@@ -0,0 +1,9 @@
+console.log("Hello world!");
+
+setTimeout(()=>{
+    console.log("I am in set timeout!");
+},1000); //will take 1 sec gap to print
+
+console.log(__dirname); //will print the directory
+console.log(__filename); //will print the filename
+
