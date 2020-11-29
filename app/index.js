e06e24967d580de08d10e7bc48e59afdc0978747
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-11-29 23:43:16 +0600

    Installing nodemon to run code automatically after each change

--- app/index.js
+++ app/index.js
@@ -3,3 +3,5 @@ const chalk = require('chalk');
 console.log(chalk.red.bold('This is a text editor!'))
 
 console.log(chalk.red.bold.inverse('This is a text editor!'))
+console.log(chalk.yellow.bold('This is a text editor!'))
+console.log(chalk.blue('This is a text editor!'))
