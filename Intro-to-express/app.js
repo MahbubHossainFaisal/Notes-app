6f52d0e9b6cfb350cfe1af053177804d22bb97a1
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-11-30 23:44:11 +0600

    First express server

--- Intro-to-express/app.js
+++ Intro-to-express/app.js
@@ -1,3 +1,10 @@
 const express = require('express');
 
-console.log(express);
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,()=> {
+
+    console.log(`Server is running on port ${PORT}`);
+})
