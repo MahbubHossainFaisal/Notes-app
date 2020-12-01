1fa5e30d9eff6fc3d1428b7bb8bc33db32f558bc
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-12-01 23:58:22 +0600

    Handling first route

--- Intro-to-express/app.js
+++ Intro-to-express/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const app = express();
-
+app.get('/',(req,res)=>{
+    res.send('<h1> Welcome to the site! </h1>')
+})
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,()=> {
