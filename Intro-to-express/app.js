3515f75b17c619ffa455917424deecbbee915981
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-12-02 23:09:00 +0600

    Creating multiple routes and handling routes that has not been declared

--- Intro-to-express/app.js
+++ Intro-to-express/app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/contact',(req,res)=>{
+    res.send('<h1> Welcome to the contact page! </h1>')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('<h1> Welcome to the about page </h1>')
+})
+
+// The root router must be declared in the 2nd last always 
 app.get('/',(req,res)=>{
     res.send('<h1> Welcome to the site! </h1>')
 })
+
+//Any route that has not been declare
+app.get('*',(req,res)=>{
+    res.send('<h1> 404 Not Found! </h1>')
+})
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,()=> {
