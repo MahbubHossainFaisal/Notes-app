2f49c24a7294431b0eb6b3e9f9072ad1c1d3c1d4
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-12-12 23:48:23 +0600

    Extracting route params from browser with params method

--- Intro-to-express/app.js
+++ Intro-to-express/app.js
@@ -8,6 +8,19 @@ const app = express();
 
 app.use('/user',userRouter) 
 
+//defining a dynamic route 
+app.get('/products/:prodId',(req,res)=>{
+   //console.log(req.params); //this will return the prod id object value which we have used in the browser for searching
+    res.send('It\'s a specific product '+ req.params.prodId);
+})
+
+//Multiple route with route id can also be declared
+app.get('/menu/:menuId/dishes/:dishId',(req,res)=> {
+    console.log(req.params)
+    
+})
+
+
 
 // The root router must be declared in the 2nd last always 
 app.get('/',(req,res)=>{
