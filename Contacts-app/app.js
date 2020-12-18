6c75be40c240f2ca3105fa15a2b424e058b50e7b
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-12-18 23:25:17 +0600

    creating contactRoute file and importing it in app.js

--- Contacts-app/app.js
+++ Contacts-app/app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 
+const morgan = require('morgan')
+
+const contactRoutes = require('./contactRoutes')
+
+app.use('/contacts',contactRoutes)
+
+app.get('*',(req,res) => {
+    res.send(<h1> Please use the correct Routes </h1>)
+})
+
 const app = express()
+
+app.use(morgan('dev'))
+
 
 const PORT = process.env.PORT || 8080
 
