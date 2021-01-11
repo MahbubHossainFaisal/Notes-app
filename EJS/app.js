c9700d89274016f167ad183369ad5a793f998e61
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-11 12:46:24 +0600

    Creating multipage in ejs

--- EJS/app.js
+++ EJS/app.js
@@ -13,6 +13,13 @@ app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
+app.get('/about', (req,res) => {
+    res.render('pages/about')
+})
+app.get('/help', (req,res) => {
+    res.render('pages/help')
+})
+
 app.get('/',(req,res) => {
     let post = {
         Title: 'Demo-Title',
@@ -24,7 +31,7 @@ app.get('/',(req,res) => {
         {title: 'Introduction to C++', author: 'Monwar Hossain' },
         {title: 'Introduction to Java', author: 'Anwar Hossain'}
     ]
-    res.render('index', {title: 'EJS - IS AWESOME TEMPLATE ENGINE',post,books})
+    res.render('pages/index', {title: 'EJS - IS AWESOME TEMPLATE ENGINE',post,books})
 })
 
 
