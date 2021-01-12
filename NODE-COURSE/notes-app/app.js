c91cb7d14d06c4fbde025a0521c4ab1d87a1302c
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-13 00:01:39 +0600

    Validator package installing and learning isEmail and isURL methods of validator

--- NODE-COURSE/notes-app/app.js
+++ NODE-COURSE/notes-app/app.js
@@ -1,12 +1,26 @@
-const add = require('./utils.js')
+// const add = require('./utils.js')
 
-const sum = add(5,4)
+// const sum = add(5,4)
 
-console.log(sum)
+// console.log(sum)
 
 
-const getNotes = require('./notes.js')
+// const getNotes = require('./notes.js')
 
-const note = getNotes()
+// const note = getNotes()
 
-console.log(note)
+// console.log(note)
+
+const validator = require('validator')
+
+console.log(validator.isEmail('mahbub@gmail.com')) // will return true
+
+console.log(validator.isEmail('13m_a@gmail.com')) // will return true
+
+console.log(validator.isEmail('mahbubyahoo.com')) // will return false
+
+//Another validator method 
+
+console.log(validator.isURL('www.facebook.com')) //will return true
+console.log(validator.isURL('http://www.facebook.com')) //will return true
+console.log(validator.isURL('https//www.facebook.com')) // will return false
