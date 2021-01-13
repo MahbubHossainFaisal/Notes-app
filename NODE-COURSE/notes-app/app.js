3096aca4f541aa0a098e161e3a96d6a60ab5e636
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-13 21:08:45 +0600

    Installing yargs and using it to create commands

--- NODE-COURSE/notes-app/app.js
+++ NODE-COURSE/notes-app/app.js
@@ -1,21 +1,38 @@
 const chalk = require('chalk')
-
+const yargs = require('yargs')
 
  const getNotes = require('./notes.js')
 
  const note = getNotes()
 
- console.log(note)
+ yargs.command({
+     command: 'add',
+     describe: 'Add a new note',
+     handler: function () { 
+         console.log('Adding a new note')
+      }
+ })
+ yargs.command({
+     command: 'remove',
+     describe: 'Remove a note',
+     handler: function () { 
+         console.log('Removing a new note')
+      }
+ })
+ yargs.command({
+     command: 'list',
+     describe: 'List all notes',
+     handler: function () { 
+         console.log('Listing all notes')
+      }
+ })
+ yargs.command({
+     command: 'read',
+     describe: 'Read a note',
+     handler: function () { 
+         console.log('Reading a note')
+      }
+ })
 
-
- console.log(chalk.green.bold.underline('Success'))
-
-
-const command = process.argv[2]
-
-if(command === 'add'){
-    console.log('adding note')
-} else if(command === 'remove') {
-    console.log('removing notes')
-}
-
+ console.log(yargs.argv)
+ 
