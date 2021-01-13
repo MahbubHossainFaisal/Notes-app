dbec78ff5246df7feebf77cdb7593cf2299cdbce
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-13 14:30:43 +0600

    Taking user input through command line

--- NODE-COURSE/notes-app/app.js
+++ NODE-COURSE/notes-app/app.js
@@ -11,5 +11,11 @@ const chalk = require('chalk')
  console.log(chalk.green.bold.underline('Success'))
 
 
+const command = process.argv[2]
 
+if(command === 'add'){
+    console.log('adding note')
+} else if(command === 'remove') {
+    console.log('removing notes')
+}
 
