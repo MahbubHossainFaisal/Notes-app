477daea9a6ed3828d69bfbb09e2df8b32ad3c867
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-14 15:20:18 +0600

    Reading from a file and parsing it to json object

--- NODE-COURSE/Exploring-topics/json.js
+++ NODE-COURSE/Exploring-topics/json.js
@@ -1,11 +1,26 @@
 
 const fs = require('fs')
-const book = {
-    title: 'Ratul er raat din',
-    author: 'Dr. Muhammad Zafar Iqbal'
-}
+
+// const book = {
+//     title: 'Ratul er raat din',
+//     author: 'Dr. Muhammad Zafar Iqbal'
+// }
 
 
-const bookJSON = JSON.stringify(book)  // It will convert the book object into string
+// const bookJSON = JSON.stringify(book)  // It will convert the book object into string
 
-fs.writeFileSync('book.json',bookJSON)
+// fs.writeFileSync('book.json',bookJSON)
+
+const dataBuffer = fs.readFileSync('book.json')
+console.log(dataBuffer) // We will get the data as sequence of numbers
+
+// to get the string back we have to use
+const dataJSON = dataBuffer.toString()
+console.log(dataJSON)
+
+
+//Now we can parse that string to be object again
+const data = JSON.parse(dataJSON)
+console.log(data.title)
+console.log(data.author)
+
